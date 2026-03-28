3243cac6db161ed720edefa20ab3d859e0cb5a1e
hak53q <hak53q@gmail.com>
2026-03-28 14:49:45 +0800

    update

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,5 @@
 import { Footer, Layout, Navbar } from 'nextra-theme-docs'
-import { Banner, Head } from 'nextra/components'
+import { Head } from 'nextra/components'
 import { getPageMap } from 'nextra/page-map'
 import 'nextra-theme-docs/style.css'
  
@@ -8,7 +8,6 @@ export const metadata = {
   // For more information on metadata API, see: https://nextjs.org/docs/app/building-your-application/optimizing/metadata
 }
  
-const banner = <Banner storageKey="some-key">Nextra 4.0 is released 🎉</Banner>
 const navbar = (
   <Navbar
     logo={<b>Nextra</b>}
@@ -34,10 +33,9 @@ export default async function RootLayout({ children }) {
       </Head>
       <body>
         <Layout
-          banner={banner}
           navbar={navbar}
           pageMap={await getPageMap()}
-          docsRepositoryBase="https://github.com/shuding/nextra/tree/main/docs"
+          docsRepositoryBase="https://github.com/hak53q/hak53q.github.io"
           footer={footer}
           // ... Your additional layout options
         >
