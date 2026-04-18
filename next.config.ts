5d7192d25f5e401f741116a3e130f9285d31f6ed
hak53q <hak53q@gmail.com>
2026-04-18 11:03:18 +0800

    update

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import nextra from 'nextra'
 const withNextra = nextra({})
 
 export default withNextra({
-  reactStrictMode: true
-  output: "export"
+  reactStrictMode: true,
+  output: "export",
   images: {
     unoptimized: true
   }
